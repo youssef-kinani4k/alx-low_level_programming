38942c33be63c67292bd3a0fadbafb4d4cb62846
youssef-kinani4k <132583862+youssef-kinani4k@users.noreply.github.com>
2023-06-15 14:37:19 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * jjjj
+ * main - Entry point
  *
- * jjj
+ * Return: Always 0 (Success)
  */
-
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
