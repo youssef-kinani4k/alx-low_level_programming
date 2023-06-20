ee88e6c914d4e8b3b16ec549a1cc1ce341999a0b
youssef-kinani4k <132583862+youssef-kinani4k@users.noreply.github.com>
2023-06-20 16:48:34 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar(43);
+_putchar('+');
 return (1);
 }
 else if (n == 0)
@@ -20,7 +20,7 @@ return (0);
 }
  else if (n < 0)
 {
-_putchar(45);
+_putchar('-');
 return (-1);
 }
 _putchar('\n');
