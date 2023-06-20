2e30f927252a7a74ebbd3b32c4e062e7c437734a
youssef-kinani4k <youssef.kinani2002@gmail.com>
2023-06-20 09:26:44 -0700

    somme fibonacci paire

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int u, u1, cha, sum;
+
+	u = 1;
+	u1 = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (u < 4000000 && (u % 2) == 0)
+		{
+			sum = sum + u;
+		}
+		next = u + u1;
+		u = u1;
+		u1 = cha;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
+
