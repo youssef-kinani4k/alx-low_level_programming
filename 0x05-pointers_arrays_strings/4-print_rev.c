6ded5cd99b273a3cef1d05eac8bd6b92bd2fa397
youssef-kinani4k <youssef.kinani2002@gmail.com>
2023-06-26 11:17:11 -0700

    xcvbdgdg

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+int l = 0;
+int i;
+
+while (*s != '\0')
+{
+l++;
+s++;
+}
+s--;
+for (i = longi; i > 0; i--)
+{
+_putchar(*s);
+s--;
+}
+
+_putchar('\n');
+}
